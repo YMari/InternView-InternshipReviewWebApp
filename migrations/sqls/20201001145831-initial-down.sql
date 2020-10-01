ab2075719ba33035dfaf8b56be60ed31c5a49ba9
kenneth-rosario <kenneth.rosario7@upr.edu>
2020-10-01 12:15:08 -0400

    Docker and docker-compose setup and changed migration tool to db-migrate

---
+++ migrations/sqls/20201001145831-initial-down.sql
@@ -0,0 +1,5 @@
+/* Replace with your SQL commands */
+
+
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
---
+++ migrations/sqls/20201001153643-seed-down.sql
@@ -0,0 +1,8 @@
+/* Replace with your SQL commands */
+
+
+DELETE FROM University;
+
+DELETE FROM StudyProgram;
+
+DELETE FROM Company;
