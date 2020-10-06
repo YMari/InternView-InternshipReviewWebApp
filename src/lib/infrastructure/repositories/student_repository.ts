4e7f7c6345a0682d72d04d961980cb4c1bf9ca45
jvserivera <jose.rivera233@upr.edu>
2020-10-06 15:52:51 -0400

    reorganizing code

--- src/lib/infrastructure/repositories/student_repository.ts
+++ src/lib/infrastructure/repositories/student_repository.ts
@@ -38,42 +38,43 @@ class StudentRepository implements student_interfaces.IStudentRepository {
 
     async updateStudent(st_email: string, st_target: entities.IStudentWithPassword): Promise<entities.IStudentDetailed> {
 
-    const student = await this.getStudentByEmail(st_email);
-    let update = {
-            where:{ email: st_email },
-            data: {
-                name: student.name,
-                university: { connect: {id: student.university.id} },
-                studyprogram: { connect: {id: student.studyprogram.id} },
-            },
-            select: {
-                name:true, email:true, studyprogram:true, university:true
-            }            
+        const student = await this.getStudentByEmail(st_email);
+        let update = {
+                where:{ email: st_email },
+                data: {
+                    name: student.name,
+                    university: { connect: {id: student.university.id} },
+                    studyprogram: { connect: {id: student.studyprogram.id} },
+                },
+                select: {
+                    name:true, email:true, studyprogram:true, university:true
+                }            
+            }
+            
+        if (st_target.name){
+            update.data.name = st_target.name;
         }
-    if (st_target.name){
-        update.data.name = st_target.name;
-    }
-    if (st_target.universityId){
-        update.data.university = {
-            connect: { id :st_target.universityId }
+        if (st_target.universityId){
+            update.data.university = {
+                connect: { id :st_target.universityId }
+            }
         }
-    }
-    if (st_target.studyProgramId){
-        update.data.studyprogram = {
-            connect: { id:st_target.studyProgramId }
+        if (st_target.studyProgramId){
+            update.data.studyprogram = {
+                connect: { id:st_target.studyProgramId }
+            }
         }
-    }
 
-    try{
-        const result = await db.student.update(update)
-        await db.$disconnect()
-            return result
+        try{
+            const result = await db.student.update(update)
+            await db.$disconnect()
+                return result
+        }
+        catch ( e ) {
+            await db.$disconnect()
+            return null
+        } 
     }
-    catch ( e ) {
-        await db.$disconnect()
-        return null
-    } 
-}
     
     async createStudent(st: entities.IStudentWithPassword): Promise<entities.IStudentDetailed> {
         
