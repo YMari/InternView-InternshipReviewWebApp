b0b5e48df02d711638b1a6d6ce1daa3c18dd7040
Yavier Mari <yavier.mari@upr.edu>
2020-10-21 12:33:13 -0400

    continued style on review page, added modal on report

--- src/pages/reviews/index.tsx
+++ src/pages/reviews/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Card, CardHeader, Checkbox, createStyles, Grid, makeStyles, Menu, MenuItem, Theme, Typography } from '@material-ui/core';
+import { Backdrop, Box, Button, Card, CardHeader, Checkbox, createStyles, Fade, FormControl, Grid, InputLabel, makeStyles, Menu, MenuItem, Modal, OutlinedInput, TextField, Theme, Typography } from '@material-ui/core';
 import Link from 'next/link';
 import React from 'react';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
@@ -9,14 +9,25 @@ export default function Review() {
     const classes = useStyles();
 
     const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const [open, setOpen] = React.useState(false);
 
     const handleClick = (event: React.MouseEvent<HTMLElement>) => {
       setAnchorEl(event.currentTarget);
     };
   
-    const handleClose = () => {
+    const handleCloseMenu = () => {
       setAnchorEl(null);
     };
+
+    const handleOpenModal = () => {
+        setOpen(true);
+      };
+
+    const handleCloseModal = () => {
+        setOpen(false);
+      };
+      
+    
 
     return (
         <Box className={classes.main}>
@@ -29,17 +40,15 @@ export default function Review() {
                 <Grid container direction='column' wrap="nowrap" className={classes.gridMain}>
 
                     <Grid container direction='column' wrap="nowrap">
-                        <Grid container direction='row' wrap="nowrap">
-                            <Link href="">    
-                                <Button>                
-                                    <Grid item>
+                        <Grid container direction='row' alignItems="center" wrap="nowrap">
+                            <Grid item>
+                                <Link href="">    
+                                    <Button>                
                                         <AccountCircleIcon fontSize="large"/>
-                                    </Grid>
-                                    <Grid item>
-                                        <Typography>Student Name</Typography>
-                                    </Grid>                          
-                                </Button>
-                            </Link>
+                                        <Typography className={classes.name}>Student Name</Typography>
+                                    </Button>
+                                </Link>
+                            </Grid>
                         </Grid>
                         <Grid item>
                             <Link href="">
@@ -49,28 +58,34 @@ export default function Review() {
                             </Link>
                         </Grid>
 
-                        <Grid container direction='row' wrap="nowrap">
+                        <Grid container direction='row' alignItems="center" wrap="nowrap" justify='space-between' className={classes.checklist}>
                             <Grid item>
                                 <Typography>Date</Typography>
                             </Grid>
-                            <Grid container direction='row' wrap="nowrap">
-                                <Checkbox disabled/>
-                                <Typography>No offer</Typography>  
+                            <Grid item>
+                                <Grid container direction='row' alignItems="center" wrap="nowrap">
+                                    <Checkbox disabled/>
+                                    <Typography>No offer</Typography>  
+                                </Grid>
                             </Grid>
-                            <Grid container direction='row' wrap="nowrap">
-                                <Checkbox disabled/>
-                                <Typography>Accepted but declined</Typography>
-                            </Grid> 
-                            <Grid container direction='row' wrap="nowrap">
-                                <Checkbox disabled checked/>
-                                <Typography>Worked</Typography> 
+                            <Grid item>
+                                <Grid container direction='row' alignItems="center" wrap="nowrap">
+                                    <Checkbox disabled/>
+                                    <Typography>Accepted but declined</Typography>
+                                </Grid>
+                            </Grid>
+                            <Grid item> 
+                                <Grid container direction='row' alignItems="center" wrap="nowrap">
+                                    <Checkbox disabled checked/>
+                                    <Typography>Worked</Typography> 
+                                </Grid>
                             </Grid>
                         </Grid>
                     </Grid>
 
                     <Grid item>
                         <Card className={classes.cardInner1}>
-
+                            
                             <Grid container direction='row' alignItems="center" wrap="nowrap">
                                 <Grid container direction='column' alignItems="center" wrap="nowrap">
                                     <Grid item>
@@ -93,61 +108,122 @@ export default function Review() {
                                 </Grid>
                             </Grid>
 
-                            <Card className={classes.cardInner2}>
-                                <CardHeader className={classes.cardSubtitle} title="Interview"/>
-                                <Grid item >
-                                    <Button
-                                    aria-controls="customized-menu"
-                                    aria-haspopup="true"
-                                    variant="contained"
-                                    color="primary"
-                                    onClick={handleClick}
-                                    >
-                                    Interview Questions
-                                    </Button>
-                                    <Menu
-                                    anchorEl={anchorEl}
-                                    keepMounted
-                                    open={Boolean(anchorEl)}
-                                    onClose={handleClose}
-                                    >
-                                        <MenuItem onClick={handleClose}>Q1</MenuItem>
-                                        <MenuItem onClick={handleClose}>Q2</MenuItem>
-                                        <MenuItem onClick={handleClose}>Q3</MenuItem>
-                                    </Menu>
-                                </Grid>
-                                <Grid item >
-                                    <Typography>Recommendations and Tips</Typography>
-                                </Grid>
-                                <Grid container direction='row' alignItems="center" wrap="nowrap">
-                                    <Typography>Interview Difficulty: </Typography>
-                                    <GradeIcon fontSize="small"/>
-                                    <GradeIcon fontSize="small"/>
-                                    <GradeIcon fontSize="small"/>
-                                    <GradeIcon fontSize="small"/>
-                                    <GradeIcon fontSize="small"/>
-                                </Grid>
-                            </Card>
+                            <Grid container direction='column' alignItems="center" wrap="nowrap" className={classes.gridItem}>
+                                <Card className={classes.cardInner2}>
+                                    <CardHeader className={classes.cardSubtitle} title="Interview"/>
+                                    <Grid item >
+                                        <Button
+                                        aria-controls="customized-menu"
+                                        aria-haspopup="true"
+                                        variant="contained"
+                                        color="primary"
+                                        onClick={handleClick}
+                                        fullWidth={true}
+                                        className={classes.buttonQuestions}
+                                        >
+                                        Interview Questions
+                                        </Button>
+                                        <Menu
+                                        anchorEl={anchorEl}
+                                        keepMounted
+                                        open={Boolean(anchorEl)}
+                                        onClose={handleCloseMenu}
+                                        >
+                                            <MenuItem onClick={handleCloseMenu}>Q1</MenuItem>
+                                            <MenuItem onClick={handleCloseMenu}>Q2</MenuItem>
+                                            <MenuItem onClick={handleCloseMenu}>Q3</MenuItem>
+                                        </Menu>
+                                    </Grid>
+                                    <Grid item >
+                                        <Typography>Recommendations and Tips</Typography>
+                                        {/* <OutlinedInput value={'text text text'}/> */}
+                                        <FormControl fullWidth={true}>
+                                            <TextField
+                                                disabled
+                                                defaultValue={'text text text'}
+                                                variant="outlined"
+                                                InputProps={{
+                                                    classes: {
+                                                        root: classes.inputRoot,
+                                                        disabled: classes.disabled
+                                                    }   
+                                                }}
+                                            />
+                                        </FormControl>
+                                    </Grid>
+                                    <Grid container direction='row' alignItems="center" wrap="nowrap">
+                                        <Typography>Interview Difficulty: </Typography>
+                                        <GradeIcon fontSize="small"/>
+                                        <GradeIcon fontSize="small"/>
+                                        <GradeIcon fontSize="small"/>
+                                        <GradeIcon fontSize="small"/>
+                                        <GradeIcon fontSize="small"/>
+                                    </Grid>
+                                </Card>
+                            </Grid>
 
                         </Card>
 
                         <Grid container direction='row' alignItems="center" wrap="nowrap">
-                            <Grid container direction='row'>
+                            <Grid container direction='row' alignItems="center">
                                 <Button>
                                     <ArrowDownward fontSize="large"/>
                                 </Button>
                                 <Typography># Downvotes</Typography>
                             </Grid>
-                            <Grid container direction='row'>
+                            <Grid container direction='row' alignItems="center">
                                 <Button>
                                     <ArrowUpward fontSize="large"/>
                                 </Button>
                                 <Typography># Upvotes</Typography>
                             </Grid>
-                            <Grid item>
-                                <Button>
+                            <Grid item >
+                                <Button onClick={handleOpenModal}>
                                     <Typography>Report</Typography>
                                 </Button>
+                                <Modal
+                                className={classes.modal}
+                                open={open}
+                                onClose={handleCloseModal}
+                                closeAfterTransition
+                                BackdropComponent={Backdrop}
+                                BackdropProps={{
+                                    timeout: 500,
+                                }}
+                                >
+                                    <Fade in={open}>
+                                        <Card className={classes.modalCard}>
+                                            <Grid container direction='column' alignItems="center" wrap="nowrap">
+                                                <Grid item>
+                                                    <Typography>Why would you like to report?</Typography>
+                                                </Grid>
+                                                <Grid item>
+                                                    <FormControl className={(classes.modalInput)} variant="outlined" fullWidth={true} required={true}>
+                                                        <OutlinedInput
+                                                            // value={''}
+                                                            // onChange={handleChange('')}
+                                                        />
+                                                    </FormControl>
+                                                </Grid>
+                                                <Grid item>
+                                                    <Grid container direction='row' alignItems="center" wrap="nowrap">
+                                                        <Grid item>
+                                                            <Button>
+                                                                <Typography>Report</Typography>
+                                                            </Button>
+                                                        </Grid>
+                                                        <Grid item>
+                                                            <Button onClick={handleCloseModal}>
+                                                                <Typography>Cancel</Typography>
+                                                            </Button>
+                                                        </Grid>
+                                                    </Grid>
+                                                </Grid>
+                                            </Grid>
+                                        </Card>
+
+                                    </Fade>
+                                </Modal>
                             </Grid>
                         </Grid>
 
@@ -170,15 +246,15 @@ const useStyles = makeStyles((theme: Theme) =>
         },
         cardMain: {
             padding: theme.spacing(2),
-            // maxWidth: '90%',
-            // minWidth: '90%',
-            // minHeight: 800,
+            maxWidth: '90%',
+            minWidth: '50%',
+            minHeight: 800,
 
 
 
         },
         gridMain: {
-
+            
         },
         gridTitleItem: {
             justifyContent: 'center',
@@ -186,19 +262,50 @@ const useStyles = makeStyles((theme: Theme) =>
             alignContent: 'center'
         },
         gridItem: {
-
+            paddingBottom: theme.spacing(1)
         },
         cardTitle: {
             fontSize: 30,
         },
         cardInner1: {
-
+            backgroundColor: theme.palette.primary.main
         },
         cardInner2: {
-
+            minWidth: '97%',
+            minHeight: '97%',
+            maxWidth: '97%',
+            maxHeight: '97%',
         },
         cardSubtitle: {
 
         },
-
+        buttonQuestions: {
+            
+        },
+        checklist: {
+            paddingLeft: theme.spacing(1),
+            paddingRight: theme.spacing(20),
+        },
+        name: {
+            paddingLeft: theme.spacing(1)
+        },
+        modal:{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        modalCard: {
+            width: 600,
+            height: 400,
+            maxWidth: '90%',
+        },
+        modalInput: {
+            
+        },
+        inputRoot: {
+            "&$disabled": {
+              color: theme.palette.info.contrastText
+            }
+        },
+        disabled: {}
     }))
