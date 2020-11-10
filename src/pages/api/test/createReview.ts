ac7de82cc2be63b2a64da56898ca57cd0e6d410a
Jose D. Maldonado <josedmaldonado14@gmail.com>
2020-11-10 11:48:41 -0400

    testing createreview

--- src/pages/api/test/createReview.ts
+++ src/pages/api/test/createReview.ts
@@ -9,5 +9,38 @@ export default async function createReview(
   res: NextApiResponse
 ) {
   const ser = container.get<re.IReviewRepository>(re.R_TYPES.IReviewRepository);
-  
+
+  if (req.method === 'POST') {
+    let output = await ser.createReview(
+      {
+        content: req.body.content,
+        date: req.body.date,
+        anonymous: req.body.anonymous,
+        email: req.body.email,
+        experienceType: req.body.experienceType,
+        degreeType: req.body.degreeType,
+        score: req.body.score,
+        location: req.body.location,
+        salary: req.body.salary,
+        duration: req.body.duration, // in weeks? idk
+        interviewDifficulty: req.body.interviewDifficulty,
+        reviewTitle: req.body.reviewTitle,
+        company: req.body.company, // Might be company interface?
+        interviewQuestions: req.body.interviewQuestions
+      }
+    )
+
+    // Error checking, revise if error message is changed
+    // if ( output.status === ERROR_MESSAGE ) {
+    //   res.status(400)
+    // }
+
+    // else{
+    //     res.status(200)
+    // }
+      res.json(output) 
+  }
+   else {
+    res.status(405).json({ message: 'We only support POST' });
+  }
 }
